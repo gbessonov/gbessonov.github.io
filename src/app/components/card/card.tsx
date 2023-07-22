b29a310f9835287e79545500e7aa6283f0f857ee
Gregory <greg.bessonov@gmail.com>
2023-07-22 14:48:23 +0200

    Apply theme

--- src/app/components/card/card.tsx
+++ src/app/components/card/card.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 
 import css_classes from '../../tools/css_classes';
 
@@ -6,37 +7,36 @@ import styles from './card.module.css';
 
 type CardProps = {
 	className?: string,
-	title: string,
 	children?: React.ReactNode,
 	gh_page_link: string,
 	gh_repo_link: string,
-	img_src: string,
+	img_src: string | StaticImport,
 	img_alt: string,
 }
 
 const Card = (props: CardProps) => {
 	return (
 		<div className={css_classes(styles.card, props.className)}>
-			<header className={css_classes(styles.card__header)}>
-				<h4>
-					{props.title}
-				</h4>
-			</header>
 			<main className={css_classes(styles.card__main)}>
-				{props.children}
-				<a href={props.gh_page_link}>
+				<a
+					className={css_classes(styles.card__img_container)}
+					href={props.gh_page_link}
+				>
 					<Image
+						className={css_classes(styles.card__img)}
 						alt={props.img_alt}
 						src={props.img_src}
-						width={400}
-						height={300}
-						priority={true}
+						priority
 					/>
 				</a>
 			</main>
+			<header className={css_classes(styles.card__header)}>
+				<h3>
+					{props.children}
+				</h3>
+			</header>
 			<footer  className={css_classes(styles.card__footer)}>
-				<p><a href={props.gh_page_link}>View on GitHub pages</a></p>
-				<p><a href={props.gh_repo_link}>Check the repository</a></p>
+				<p><a href={props.gh_repo_link}>Sources</a></p>
 			</footer>
 		</div>
 	)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,38 @@
-import Image from 'next/image';
-
 import css_classes from './tools/css_classes';
 
 import Card from './components/card';
 
 import styles from './page.module.css';
 
+import mandelbrot_img from '../../public/imgs/mandelbrot.png';
+
 
 export default function Home() {
 	return (
 		<div className={styles.main_container}>
 			<header className={css_classes(styles.header, styles.section)}>
-				<h1>Welcome to the website of Gregory Bessonov</h1>
+				<h1>Welcome</h1>
+				<h2>I'm Gregory, full-stack software engineer.</h2>
 			</header>
 			<main className={css_classes(styles.main, styles.section)}>
-				<div className={css_classes(styles.main__intro)}>
-					<p>
-						Hello! My name is Gregory, I am a full-stack developer with more than 10 years of experience in the full development cycle.
-					</p>
-					<p>
-						Some of my hobby projects are presented below:
-					</p>
-				</div>
 				<Card
-					title = 'Mandelbrot set'
-					img_src = 'imgs/mandelbrot.png'
+					img_src = {mandelbrot_img}
 					img_alt = 'Mandelbrot set'
 					gh_page_link = 'https://gbessonov.github.io/mandelbrot'
 					gh_repo_link = 'https://github.com/gbessonov/mandelbrot'
 
 					className = {css_classes(styles.main__card)}
 				>
-					<p>It is pure html/js application which renders <a href="https://en.wikipedia.org/wiki/Mandelbrot_set">Mandlelbrot set</a> via <a href="https://get.webgl.org/">WebGL</a>.</p>
+					<a href="https://en.wikipedia.org/wiki/Mandelbrot_set">Mandelbrot set</a> visualisation via <a href="https://get.webgl.org/">WebGL</a>
 				</Card>
 			</main>
-			<footer className={css_classes(styles.footer, styles.section)}>
-				<h2>
-					<a href='mailto:greg.bessonov@gmail.com'>greg.bessonov@gmail.com</a>
-				</h2>
+			<footer className={css_classes(styles.footer__container, styles.section)}>
+				<div className={css_classes(styles.footer)}>
+					<p>Gregory Bessonov</p>
+					<h2>
+						<a href='https://github.com/gbessonov'>github.com/gbessonov</a>
+					</h2>
+				</div>
 			</footer>
 		</div>
 	);
