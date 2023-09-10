306504f1876f26386d8f8ca2eff49a02e000ca52
Gregory <greg.bessonov@gmail.com>
2023-09-10 22:40:56 +0200

    Game of Life

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Card from './components/card';
 import styles from './page.module.css';
 
 import mandelbrot_img from '../../public/imgs/mandelbrot.png';
+import gol_img from '../../public/imgs/GoL.png';
 
 
 export default function Home() {
@@ -15,6 +16,16 @@ export default function Home() {
 				<h2>I&apos;m Gregory, full-stack software engineer.</h2>
 			</header>
 			<main className={css_classes(styles.main, styles.section)}>
+				<Card
+					img_src = {gol_img}
+					img_alt = 'Game of Life'
+					gh_page_link = 'https://gbessonov.github.io/life'
+					gh_repo_link = 'https://github.com/gbessonov/life'
+
+					className = {css_classes(styles.main__card)}
+				>
+					<a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Conway&apos;s Game of Life</a> using <a href="https://threejs.org/">three.js</a>
+				</Card>
 				<Card
 					img_src = {mandelbrot_img}
 					img_alt = 'Mandelbrot set'
